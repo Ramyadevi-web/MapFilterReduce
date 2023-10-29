9c0d79752a37bb5542b93fb031c5dbe04fc21256
Ramyadevi-web <ramyadevim7@gmail.com>
2023-10-29 16:34:14 +0530

    Filter  USD currency countries

--- script5.js
+++ script5.js
@@ -4,7 +4,11 @@ country.open("GET","https://restcountries.com/v3.1/all");
 country.send();
 country.onload = function(){
     let data = JSON.parse(country.response);
+    // console.log(data)
+for(ele of data){
 
-    let filteredcountry = data.filter((index) => index.currencies.USD.symbol =="$");
-    console.log(filteredcountry);
+    if(ele.currencies.USD){
+        console.log(ele)
+    }
+}
 }
